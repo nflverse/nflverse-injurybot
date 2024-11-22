4b970da59a0d5e3dbba00e01724ff2fa06e4c903
mrcaseb <mrcaseb@gmail.com>
2024-11-22 15:04:28 +0100

    finish update script

--- auto/update_injuries.R
+++ auto/update_injuries.R
@@ -24,11 +24,34 @@ injury_data <- nflverse.injurybot::fetch_injuries(
   week = week
 )
 
-one_game <- nflverse.injurybot::evaluate_game("2024_12_KC_CAR", week_games, injury_data)
-nflverse.injurybot::compute_game_table(one_game, week_games[week_games$nflverse_id == "2024_12_KC_CAR",])
+# Process every game of current week
+for (game_id in week_games$nflverse_id) {
+  current_game <- nflverse.injurybot::evaluate_game(game_id, week_games, injury_data)
+  last_run <- try(nflreadr::rds_from_url(paste0(
+    "https://github.com/nflverse/nflverse-data/releases/download/",
+    "injuries_", season, "/", game_id, ".rds"
+  )), silent = TRUE)
+  game_data <- week_games[week_games$nflverse_id == game_id,]
 
-  # bskyr::bs_post(
-  #   text = compute_post_text(game_data),
-  #   images = file_name,
-  #   images_alt = compute_alt_text()
-  # )
+  # compare to last run if the file is available. If not, it's likely the first run
+  if (!inherits(last_run, "try-error")){
+    if (identical(current_game, last_run)) next
+  }
+
+  # If we reach this point, data has changed and we would like to post an update
+  tbl <- nflverse.injurybot::compute_game_table(current_game, game_data)
+  file_name <- gt::gtsave(tbl, paste0("auto/", game_id, ".png"), zoom = 4, quiet = TRUE)
+  bskyr::bs_post(
+    text = nflverse.injurybot::compute_post_text(game_data),
+    images = file_name,
+    images_alt = nflverse.injurybot::compute_alt_text()
+  )
+  # upload new data if everything worked fine until this point
+  saveRDS(current_game, paste0("auto/", game_id, ".rds"))
+  nflversedata:::gh_cli_release_upload(
+    files = paste0("auto/", game_id, ".rds"),
+    tag = paste0("injuries_", season),
+    repo = "nflverse/nflverse-injurybot"
+  )
+}
+
