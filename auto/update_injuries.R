19439f949c834d01b65cd06fbdf7da473baf63bc
mrcaseb <mrcaseb@gmail.com>
2024-11-22 15:36:49 +0100

    improved data comparison

--- auto/update_injuries.R
+++ auto/update_injuries.R
@@ -27,25 +27,29 @@ injury_data <- nflverse.injurybot::fetch_injuries(
 # Process every game of current week
 for (game_id in week_games$nflverse_id) {
   current_game <- nflverse.injurybot::evaluate_game(game_id, week_games, injury_data)
-  last_run <- try(nflreadr::rds_from_url(paste0(
-    "https://github.com/nflverse/nflverse-data/releases/download/",
-    "injuries_", season, "/", game_id, ".rds"
-  )), silent = TRUE)
+  last_run <- nflverse.injurybot::fetch_from_release(game_id)
   game_data <- week_games[week_games$nflverse_id == game_id,]
 
   # compare to last run if the file is available. If not, it's likely the first run
-  if (!inherits(last_run, "try-error")){
-    if (identical(current_game, last_run)) next
+  if (identical(current_game, last_run)){
+    cli::cli_alert_info("No new data for {.val {game_id}}")
+    next
   }
 
   # If we reach this point, data has changed and we would like to post an update
   tbl <- nflverse.injurybot::compute_game_table(current_game, game_data)
   file_name <- gt::gtsave(tbl, paste0("auto/", game_id, ".png"), zoom = 4, quiet = TRUE)
-  bskyr::bs_post(
+
+  # Just in case the code is running too fast,
+  # we take a nap here to protect bsky servers
+  Sys.sleep(3)
+  post <- bskyr::bs_post(
     text = nflverse.injurybot::compute_post_text(game_data),
     images = file_name,
     images_alt = nflverse.injurybot::compute_alt_text()
   )
+  cli::cli_alert_success("Posted update for {.val {game_id}}")
+
   # upload new data if everything worked fine until this point
   saveRDS(current_game, paste0("auto/", game_id, ".rds"))
   nflversedata:::gh_cli_release_upload(
